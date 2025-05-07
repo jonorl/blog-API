401b939c28f218c69a9d8982f16a08e6dd577145
jonorl <39122230+jonorl@users.noreply.github.com>
2025-05-07 14:41:06 +0300

    Feat: GET, POST, PUT methods for posting created

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,13 @@
+// Config
+
 const express = require("express");
 const db = require("./queries");
 const jwt = require("jsonwebtoken");
-
 const app = express();
+
+// Router
+
+// GET routes
 
 app.get("/api", (req, res) => {
   res.json({
@@ -10,15 +15,54 @@ app.get("/api", (req, res) => {
   });
 });
 
-app.post("/api/posts", verifyToken, (req, res) => {
-  jwt.verify(req.token, "secreto", (err, authData) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      res.json({ message: "post created", authData });
-    }
+app.get("/api/userDetails/:id", async (req, res) => {
+  const userPromise = db.readUser(req.params.id);
+  const user = await userPromise;
+  res.json({ message: "User details: ", user });
+});
+
+// POST routes
+
+app.post("/api/createPost", async (req, res) => {
+  // replace headers, placeholder data
+  const postPromise = db.createPost(
+    req.headers.user,
+    "My second title",
+    "something not so interesting...",
+    true
+  );
+  const newPost = await postPromise;
+  res.json({ message: "Blog posting successful", newPost });
+});
+
+app.put("/api/editPost", async (req, res) => {
+  // replace headers, placeholder data
+  const updatePromise = db.editPost(
+    parseInt(req.headers.id),
+    "Just 2nd Title",
+    "Something a bit more interesting",
+    false
+  );
+  const update = await updatePromise;
+  res.json({
+    message: "Post edited successufly",
+    update,
   });
 });
+
+app.post("/api/createUser", async (req, res) => {
+  const newUserPromise = db.insertNewUser(
+    // replace headers with body once form is created
+    req.headers.firstname,
+    req.headers.lastname,
+    req.headers.email,
+    req.headers.password
+  );
+  const newUser = await newUserPromise;
+  res.json({ message: "New user created!: ", newUser });
+});
+
+// Sample tutorial
 
 app.post("/api/login", (req, res) => {
   const user = {
@@ -30,7 +74,7 @@ app.post("/api/login", (req, res) => {
     {
       user,
     },
-    "secreto",
+    process.env.SECRET,
     { expiresIn: "30s" },
     (err, token) => {
       res.json({
@@ -40,22 +84,17 @@ app.post("/api/login", (req, res) => {
   );
 });
 
-app.get("/api/userDetails/:id", async (req, res) => {
-  const userPromise = db.readUser(req.params.id);
-  const user = await userPromise;
-  res.json({ message: "User details: ", user });
+app.post("/api/posts", verifyToken, (req, res) => {
+  jwt.verify(req.token, process.env.SECRET, (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({ message: "post created", authData });
+    }
+  });
 });
 
-app.post("/api/createUser", async (req, res) => {
-  const newUserPromise = db.insertNewUser(
-    req.headers.firstname,
-    req.headers.lastname,
-    req.headers.email,
-    req.headers.password
-  );
-  const newUser = await newUserPromise;
-  res.json({ message: "New user created!: ", newUser });
-});
+// Controllers
 
 function verifyToken(req, res, next) {
   const bearerHeader = req.headers["authorization"];
@@ -69,4 +108,4 @@ function verifyToken(req, res, next) {
   }
 }
 
-app.listen(3000, () => console.log("Listeining on port 3000"));
+app.listen(process.env.PORT, () => console.log("Listeining on port 3000"));
--- backend/queries.js
+++ backend/queries.js
@@ -2,27 +2,55 @@ const { PrismaClient } = require("./generated/prisma/client");
 const prisma = new PrismaClient();
 
 async function insertNewUser(firstName, lastName, email, password) {
-    const result = await prisma.users.create({
-        data: {
-          first_name: firstName,
-          last_name: lastName,
-          email: email,
-          password_hash: password,
-        },
-      });
-      return result;
+  const result = await prisma.users.create({
+    data: {
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+      password_hash: password,
+    },
+  });
+  return result;
 }
 
 async function readUser(id) {
-    const result = await prisma.users.findUnique({
-        where: {
-          user_id: id,
-        },
-      });
-      return result;
+  const result = await prisma.users.findUnique({
+    where: {
+      user_id: id,
+    },
+  });
+  return result;
+}
+
+async function createPost(user, title, text, isPublished) {
+  const post = await prisma.posts.create({
+    data: {
+      author_id: user,
+      title: title,
+      post_text: text,
+      is_published: isPublished,
+    },
+  });
+  return post;
+}
+
+async function editPost(id, title, text, isPublished) {
+  const update = await prisma.posts.update({
+    where: {
+      post_id: id,
+    },
+    data: {
+      title: title,
+      post_text: text,
+      is_published: isPublished,
+    },
+  });
+  return update;
 }
 
 module.exports = {
   insertNewUser,
   readUser,
+  createPost,
+  editPost,
 };
