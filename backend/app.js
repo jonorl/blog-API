b03885efcfcb8e815e0be2a22cc39e2bc74c5b15
jonorl <39122230+jonorl@users.noreply.github.com>
2025-05-08 12:06:21 +0300

    WIP: User authentication using jwt

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,44 @@
 // Config
 
+// Express setup
 const express = require("express");
-
 const app = express();
 
-// Load Routers
+// Router triggering
 const mainRouter = require("./routes/mainRouter");
 app.use("/", mainRouter);
 
-app.listen(process.env.PORT, () => console.log("Listeining on port 3000"));
+// Passport / JWT setup
+
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const db = require("./db/queries");
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.SECRET,
+};
+
+// Create JWT strategy
+const jwtLogin = new JwtStrategy(jwtOptions, async (payload, done) => {
+  try {
+    const user = await db.readUser(payload.sub);
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  } catch (err) {
+    return done(err, false);
+  }
+});
+
+passport.use(jwtLogin);
+app.use(passport.initialize())
+
+// Launch and port confirmation
+app.listen(process.env.PORT, () =>
+  console.log(`Listeining on port ${process.env.PORT}`)
+);
