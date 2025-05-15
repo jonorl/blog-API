586bbb536aaa1f151f01b69d35cec8692b53e0df
jonorl <39122230+jonorl@users.noreply.github.com>
2025-05-15 15:27:47 +0300

    WIP: Fixing fly.io not redirecting to other pages

--- user-frontend/vite.config.js
+++ user-frontend/vite.config.js
@@ -1,17 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import * as path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import * as path from 'path';
 
 export default defineConfig({
+  build: {
+    outDir: 'dist', // Must match Nginx's root directory
+    emptyOutDir: true,
+  },
+  // .
   plugins: [react(), tailwindcss()],
-    server: {
+  server: {
     host: '0.0.0.0',
-    port: 8080
+    port: 8080,
   },
   resolve: {
     alias: {
-      '@': path.resolve(path.dirname(''), 'src') 
-    }
-  }
-})
+      '@': path.resolve(path.dirname(''), 'src'),
+    },
+  },
+});
