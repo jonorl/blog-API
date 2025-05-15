a81e5de4b80213f1df9428172adaf3d23206e12d
jonorl <39122230+jonorl@users.noreply.github.com>
2025-05-15 14:48:13 +0300

    added 0.0.0.0 to the vite config.

--- user-frontend/vite.config.js
+++ user-frontend/vite.config.js
@@ -5,6 +5,10 @@ import * as path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    server: {
+    host: '0.0.0.0',
+    port: 8080
+  },
   resolve: {
     alias: {
       '@': path.resolve(path.dirname(''), 'src') 
